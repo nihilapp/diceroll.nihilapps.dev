b718d3af18582e65417c14a0a037fb6bbf67f560
NIHILncunia <nihil_ncunia@naver.com>
2024-06-29 06:41:30 +0900

    2024 0629 필요 없는 파일 제거, 미세 수정

--- src/data/config.data.ts
+++ src/data/config.data.ts
@@ -1,9 +1,9 @@
 import { IConfigData } from '@/src/entities';
 
 export const configData: IConfigData = {
-  title: '사이트 이름',
-  description: '사이트 설명',
-  keywords: '',
+  title: 'DiceRoll',
+  description: '언제 어디서든 편하게 주사위를 굴리세요.',
+  keywords: 'Dice,주사위,DND,roll dice',
   author: {
     name: 'NIHILncunia',
     url: 'https://github.com/NIHILncunia',
